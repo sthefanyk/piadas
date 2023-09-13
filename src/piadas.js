1cc93dfbce47d7856f595b82fff6f1cc9291bba3
sthefanyk <sthefanykmbrl@gmail.com>
2023-09-13 09:39:27 -0300

    atv feita

--- src/piadas.js
+++ src/piadas.js
@@ -9,29 +9,23 @@ let jokeData;
 function getJoke() {
     axios.get('https://v2.jokeapi.dev/joke/Any?lang=pt').then(response => {
         responseElement.textContent = "Resposta";
+        dogImageElement.innerHTML = '';
         jokeData = response.data;
 
         jokeElement.textContent = jokeData.setup;
-    })
-}
-
-function getReact() {
-    axios.get('https://dog.ceo/api/breeds/image/random').then(response => {
-        responseElement.textContent = "Resposta";
-        jokeData = response.data;
-
-        jokeElement.textContent = jokeData.setup;
-    })
+    }).catch(error => console.log("Erro ao buscar a piada" + error))
 }
 
 function getResponse() {
     responseElement.textContent = jokeData.delivery;
+    getDogImage();
 }
 
-function getImage() {
+function getDogImage() {
     axios.get('https://dog.ceo/api/breeds/image/random').then(response => {
         const urlImage = response.data.message;
+        console.log(urlImage);
 
-        jokeElement.textContent = jokeData.setup;
-    })
+        dogImageElement.innerHTML = `<img src="${urlImage}" alt="Cachorro engraçado">`
+    }).catch(error => console.log("Erro ao buscar a imagem" + error));
 }
